153550aa28745d908ee889deada18ceef7928220
Javier Merchán <merchan1395@gmail.com>
2016-08-04 17:37:30 -0500

    arreglo de nabab general para todas las paga.

---
+++ compty-admin-navbar.php
@@ -0,0 +1,23 @@
+<nav class="navbar navbar-default navbar-fixed">
+    <div class="container-fluid">
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle" data-toggle="collapse">
+                <span class="sr-only" style="color:white">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="index.php"><img src="resources/images/logo.png" alt=""
+                style="max-width: 150px; max-height: 150px; float: left; margin: auto;"/></a>
+        </div>
+        <div class="collapse navbar-collapse">
+            <ul class="nav navbar-nav navbar-right">
+                <li>
+                   <a href="compty-admin.php" style="color: white">
+                            Log out
+                    </a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
--- compty-admin-perfil_usuario.php
+++ compty-admin-perfil_usuario.php
@@ -42,29 +42,8 @@
     <?php include("compty-admin-sidebar.php"); ?>
 
     <div class="main-panel">
-        <nav class="navbar navbar-default navbar-fixed">
-            <div class="container-fluid">
-                <div class="navbar-header">
-                    <button type="button" class="navbar-toggle" data-toggle="collapse">
-                        <span class="sr-only" style="color:white">Toggle navigation</span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                    </button>
-                    <a class="navbar-brand" href="index.php"><img src="resources/images/logo.png" alt=""
-						style="max-width: 150px; max-height: 150px; float: left; margin: auto;"/></a>
-                </div>
-                <div class="collapse navbar-collapse">
-                    <ul class="nav navbar-nav navbar-right">
-                        <li>
-                           <a href="compty-admin.php" style="color: white">
-                               		Log out
-                            </a>
-                        </li>
-                    </ul>
-                </div>
-            </div>
-        </nav>
+
+		<?php include("compty-admin-navbar.php"); ?>
 
 		<div class="content">
             <div class="container-fluid">
