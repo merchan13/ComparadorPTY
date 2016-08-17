eeabbec8e366436ea5755cdd9fbda5c5d7a803f7
Javier Merchán <merchan1395@gmail.com>
2016-08-16 23:17:34 -0500

    Comienzo de Responsive Design
    go go go

--- compty-admin.php
+++ compty-admin.php
@@ -4,6 +4,9 @@
 		<meta charset="utf-8">
 		<title>ComparadorPty - Login</title>
 
+		<meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0' name='viewport' />
+	    <meta name="viewport" content="width=device-width" />
+
 		<!-- Bootstrap core CSS -->
 		<link href="vendors/bootstrap/css/bootstrap.css" rel="stylesheet">
 
