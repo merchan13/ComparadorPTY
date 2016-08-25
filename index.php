2d4a41ac2f2c2fc1ff305e8057b2716961233de6
Javier Merchán <merchan1395@gmail.com>
2016-08-25 18:18:06 -0500

    Alertas post formulario
    .

--- index.php
+++ index.php
@@ -9,12 +9,17 @@
 
 		<!-- Bootstrap core CSS -->
 		<link href="vendors/bootstrap/css/bootstrap.css" rel="stylesheet">
+		<link href="http://maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
+	    <link href='http://fonts.googleapis.com/css?family=Roboto:400,700,300' rel='stylesheet' type='text/css'>
+		<link href="resources/assets/css/animate.min.css" rel="stylesheet"/>
 
 		<!--Styles-->
 		<link rel="stylesheet" type="text/css" href="resources/css/style.css">
 		<!--/Styles-->
 
 		<!--JavaScript-->
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+		<script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 		<!--/JavaScript-->
 
 	</head>
@@ -32,6 +37,38 @@
 			</header>
 
 			<div class="products">
+					<?php
+						if (isset($_GET['send'])){
+							if ($_GET['send'] == 666){
+
+								echo'
+								<div class="login-alert-error">
+									<div  class="alert alert-success fade in">
+										<a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>
+										<strong>Éxito!</strong>
+										 Solicitud de producto enviada exitosamente!
+										 <br>
+										 Se le estará contactando lo más
+										 pronto posible, lo invitamos a seguir comparando, gracias!
+										 <i class="fa fa-check-circle fa-ex" aria-hidden="true"></i>
+									</div>
+								</div>
+								';
+							} else if ($_GET['send'] == 999) {
+								echo'
+								<div class="login-alert-error">
+									<div  class="alert alert-danger fade in">
+										<a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>
+										<strong>Oh no!</strong>
+										 Ocurrió un error en el proceso, por favor intente de nuevo.
+										 <br>
+										 <i class="fa fa-frown-o fa-ex" aria-hidden="true"></i>
+									</div>
+								</div>
+								';
+							}
+						}
+					?>
 					<div class="title">
 							<h1>Elija un producto para comparar</h1>
 							<br>
